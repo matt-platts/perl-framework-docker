49d43dc7d9428786a4d42f8bc1b029a7f7abb220
Matt Platts <matt.platts@broadcom.com>
2021-04-17 19:54:39 +0100

    Tidying up

--- test_environment/scripts/test.pl
+++
@@ -1,9 +0,0 @@
-$url="http://here.com?gclid=98734095873405987345&action=1";
-
-$url =~ s/\?gclid=[a-zA-Z0-9]*&/\?/;
-
-print $url;
-
-$curUrl = $params['currenturl'];
-$pattern = "/\?gclid=[a-zA-Z0-9]*&/";
-$curUrl = preg_replace($pattern, "?", $curUrlr);
